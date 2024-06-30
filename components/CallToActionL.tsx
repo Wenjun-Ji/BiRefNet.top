c5d09969871d7641cf0bbb1b075d3a3ae4dc176e
jiangjiahao0208 <1748492421@qq.com>
2024-06-30 21:09:10 +0800

    Update CallToAction

--- components/CallToActionL.tsx
+++ components/CallToActionL.tsx
@@ -1,14 +1,13 @@
-// components/CallToActionL.js
 import Image from 'next/image';
 
 const CallToActionL = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between bg-white p-8 rounded-lg shadow-md">
-      <div className="text-center md:text-left md:w-1/2 md:pr-8">
-        <h2 className="text-3xl font-bold text-gray-900">
+    <div className="flex flex-col md:flex-row items-center justify-between bg-white p-8 mb-8">
+      <div className="text-center md:text-left md:w-1/2 md:pr-16">
+        <h2 className="text-3xl font-bold text-gray-900 max-w-[500px] mx-auto">
           使用AI即时创建背景
         </h2>
-        <p className="text-lg text-gray-700 mt-4">
+        <p className="text-lg text-gray-700 mt-4 max-w-[500px] mx-auto">
           Photoroom利用AI的力量在几秒钟内为您的产品图片创建逼真、具有工作室质量的背景。或者试试我们的文本到图像工具，从你的想象力提示中即时生成视觉效果。
         </p>
         <a
--- components/CallToActionR.tsx
+++ components/CallToActionR.tsx
@@ -1,10 +1,9 @@
-// components/CallToActionR.js
 import Image from 'next/image';
 
 const CallToActionR = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between bg-white p-8 rounded-lg shadow-md">
-      <div className="mt-8 md:mt-0 md:w-1/2">
+    <div className="flex flex-col md:flex-row items-center justify-between bg-white p-8 mb-8">
+      <div className="mt-8 md:mt-0 md:w-1/2" >
         <Image
           src="/og-image.png"  
           alt="AI Generated Backgrounds"
@@ -13,11 +12,11 @@ const CallToActionR = () => {
           className="rounded-lg"
         />
       </div>
-      <div className="text-center md:text-left md:w-1/2 md:pl-8">
-        <h2 className="text-3xl font-bold text-gray-900">
+      <div className="text-center md:text-left md:w-1/2 md:pl-16">
+        <h2 className="text-3xl font-bold text-gray-900 max-w-[500px] mx-auto">
           使用AI即时创建背景
         </h2>
-        <p className="text-lg text-gray-700 mt-4">
+        <p className="text-lg text-gray-700 mt-4 max-w-[500px] mx-auto">
           Photoroom利用AI的力量在几秒钟内为您的产品图片创建逼真、具有工作室质量的背景。或者试试我们的文本到图像工具，从你的想象力提示中即时生成视觉效果。
         </p>
         <a
